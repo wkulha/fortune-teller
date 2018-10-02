6263a8c86145b8a1ed5816b9ad4d921b0c86a61f
William Kulha <kulha.william@gmail.com>
2018-10-02 14:43:53 -0400

    Create two custom functions: one to gather, and compare user input; and the other to then take the results ofthat comparison, change the background color, hide the quiz, and show their fortune.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//Gather user input and place it into an array.
+function hideShowDarken(divToShow) {
+  $('#the_quiz').hide();
+  $('.well').hide();
+  $('body').addClass('seance-dark');
+  $(divToShow).slideDown();
+}
+
+function gatherAndCompare() {
+  let goodLuckTotal = [];
+  $("input:checkbox[name=good_luck]:checked").each(function() {
+      let currentAnswer = $(this).val();
+      goodLuckTotal.push(currentAnswer);
+  });
+  let badLuckTotal = [];
+  $("input:checkbox[name=bad_luck]:checked").each(function() {
+    let currentAnswer = $(this).val();
+    badLuckTotal.push(currentAnswer);
+  });
+  if (badLuckTotal.length >= 5 && goodLuckTotal.length <= 2) {
+    hideShowDarken('#perfect_fortune');
+  } else if (badLuckTotal.length > goodLuckTotal.length) {
+    hideShowDarken('#good_fortune');
+  } else if (goodLuckTotal.length > badLuckTotal.length) {
+    hideShowDarken('#bad_fortune');
+  }
+}
+
+$(document).ready(function() {
+  $('#quiz').submit(function(event) {
+    event.preventDefault();
+    gatherAndCompare();
+  });
+});
